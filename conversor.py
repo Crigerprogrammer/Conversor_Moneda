881fb2393aa1b98a4bf6638b0a6ade67765a8dbd
Cristian <crigerprogrammer@gmail.com>
2020-12-09 23:34:34 -0600

    agregando operacion dolares entre quetzalez

--- conversor.py
+++ conversor.py
@@ -1,2 +1,6 @@
 quetzalez = input("¿Cuántos quetzalez tienes?: ")
 quetzalez = float(quetzalez)
+valor_dolar = 7.83
+dolares = quetzalez / valor_dolar
+dolares = str(dolares)
+print("Tienes $ " + dolares + " dolares")

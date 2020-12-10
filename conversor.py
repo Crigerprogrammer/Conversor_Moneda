31bfae46957bfba74cd07c7775bef75105c6ff38
Cristian <crigerprogrammer@gmail.com>
2020-12-09 23:42:49 -0600

    agregando conversor de dolares a quetzales tambien

--- conversor.py
+++
@@ -1,7 +0,0 @@
-quetzalez = input("¿Cuántos quetzalez tienes?: ")
-quetzalez = float(quetzalez)
-valor_dolar = 7.83
-dolares = quetzalez / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $ " + dolares + " dolares")

d2dfdfda80a27f976a0b83226a96c80b588645a6
Cristian <crigerprogrammer@gmail.com>
2020-12-09 23:45:28 -0600

    nuevos archivos

---
+++ conversor_dolares.py
@@ -0,0 +1,7 @@
+dolares = input("Cuantos dolares tienes?: ")
+dolares = float(dolares)
+valor_quetzal = 0.13
+quetzalez = dolares / valor_quetzal
+quetzalez = round(quetzalez, 2)
+quetzalez = str(quetzalez)
+print("Tienes Q" + quetzalez + " quetzalez")

0255b98ae166a464383c4d0bf2007c98aa1dfc07
Cristian <crigerprogrammer@gmail.com>
2020-12-10 07:42:17 -0600

    agregando condicional al programa de conversion

--- conversor_quetzalez.py
+++ conversor_quetzalez.py
@@ -1,7 +1,38 @@
-quetzalez = input("¿Cuántos quetzalez tienes?: ")
-quetzalez = float(quetzalez)
-valor_dolar = 7.83
-dolares = quetzalez / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $ " + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas 😜
+
+1 - Quetzales
+2 - Pesos Argentinos
+3 - Pesos Mexicanos
+
+Elige una opción"""
+
+opcion = input(menu)
+
+if opcion == '1':
+    quetzalez = input("¿Cuántos quetzalez tienes?: ")
+    quetzalez = float(quetzalez)
+    valor_dolar = 7.83
+    dolares = quetzalez / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+elif opcion == '2':
+    quetzalez = input("¿Cuántos pesos argentinos tienes?: ")
+    quetzalez = float(quetzalez)
+    valor_dolar = 65
+    dolares = quetzalez / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+elif opcion == '3':
+    quetzalez = input("¿Cuántos pesos mexicanos tienes?: ")
+    quetzalez = float(quetzalez)
+    valor_dolar = 24
+    dolares = quetzalez / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+else:
+    print("Favor colocar una opción válida")
+

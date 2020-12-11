6b9828571ba52a6d8c51002e75a715ea94cca06f
Cristian <crigerprogrammer@gmail.com>
2020-12-11 15:45:09 -0600

    raiz cuadrada

---
+++ iteraciones.py
@@ -0,0 +1,15 @@
+contador = 0
+
+while contador < 10:
+    print(contador)
+    contador += 1 # Esto es igual a contador = contador + 1
+
+
+contador_externo = 0
+contador_interno = 0
+
+while contador_externo <10:
+    while contador_interno <20:
+        print(contador_externo, contador_interno)
+        contador_interno += 1
+    contador_externo += 1 

535aff7d6969dd278ea86324cc47a6c98cf10f50
Cristian <crigerprogrammer@gmail.com>
2020-12-13 14:09:01 -0600

    especificaciones

---
+++ espeficicaciones_codigo.py
@@ -0,0 +1,6 @@
+def a(cualquier_parametro):
+    """Descripción de lo que hace la función
+        cualquier_parametro int cualquier entero
+        returns cualquier_parametro + 5
+    """
+    return cualquier_parametro + 5
